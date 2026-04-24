33d4bac06ba705e5c44d33e60b3486fcf5c959db
anideva <anidevarchowdhury@gmial.com>
2026-04-24 13:25:39 +0530

    feat:render subject cards dynamically from the topics data

---
+++ app.js
@@ -0,0 +1,26 @@
+//state - tracks everything about the quiz
+let currentTopic=null;
+let currentQuestionIndex=0;
+let score= 0;
+let timeInterval=null;
+let timeLeft= 15;
+
+//elements - grabiing all HTML elements once 
+const homeScreen= document.getElementById('home-screen');
+const quizScreen= document.getElementById('quiz-screen');
+const resultsScreen = document.getElementById('results-screen');
+const subjectGrid= document.getElementById('subject-grid');
+
+//init-- this runs when the page will loads first 
+function init() {
+    topics.forEach(function(topic){
+        const btn=document.createElement('button');
+        btn.textContent= topic.title;
+        btn.addEventListener('click',function(){
+            startQuiz(topic);
+        });
+        subjectGrid.appendChild(btn);
+    });
+}
+
+init()
